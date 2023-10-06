0b66f24029293b8658489c925dda019bb8523a74
Marlon Campo <barriguibela@gmail.com>
2023-10-06 14:44:21 -0500

    listado de personas creado en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'listado-personas';
+  titulo = 'Listado de personas';
+
+  personasArray: Persona[] = [
+    new Persona("juan", "perez"),
+    new Persona("Laura", "Juarez")
+  ];
+
+  nombreInput:string = '';
+  apellidoInput:string = '';
+
+  agregarPersona() {
+
+    let persona1 = new Persona(this.nombreInput,this.apellidoInput);
+    this.personasArray.push(persona1);
+    /*
+    this.personasArray.push(new Persona(this.nombreInput, this.apellidoInput));
+    this.nombreInput = '';
+    this.apellidoInput = '';
+    */
+  }
 }
